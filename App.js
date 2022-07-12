50ab5607abbada2d386e07f42dd627e2ed4f969a
Prahmodh R <89332177+Prahmodh-R@users.noreply.github.com>
2022-07-12 13:14:35 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+function App1() {
+  return (
+    <div>
+      <Heading></Heading>
+      <Note></Note>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App1;
